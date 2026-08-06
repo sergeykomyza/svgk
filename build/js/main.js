edd39c565650e20edb47c171a300b520a83b7c8f
sergekomyza <sergekomyza@gmail.com>
2026-08-06 16:47:54 +0300

    v1.0

---
+++ build/js/main.js
@@ -0,0 +1,132 @@
+// ================================================== MENU
+const menu = () => {
+    const menuToggle = document.querySelector('.js-toggleMenu')
+    menuToggle.addEventListener('click', function(){
+        this.classList.toggle('is-active')
+    })
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ПРОКРУТКА, ШАПКА
+// document.addEventListener('DOMContentLoaded', function () {
+//     // СКРОЛЛ К НУЖНОЙ СЕКЦИИ ПО КЛИКУ НА ПУНКТАХ МЕНЮ
+//     $('.menu__link').click(function () {
+//         var scroll_elem = $(this).attr('href');
+//         $('html, body').animate({
+//             scrollTop: $(scroll_elem).offset().top
+//         }, 1000);
+//     });
+//     // ДОБАВЛЯЕМ АКТИВНЫЙ КЛАСС ШАПКЕ
+//     function headerActiveToggle() {
+//         const scrollSize = window.pageYOffset
+//         scrollSize > 1 ? header.classList.add('active') : header.classList.remove('active')
+//     }
+//     window.addEventListener('load', headerActiveToggle) // ПРИ ПЕРЕЗАГРУЗКЕ СТРАНИЦЫ ЕСЛИ СТРАНИЦА УЖЕ ПРОСКРОЛЛЕНА
+//     window.addEventListener('scroll', headerActiveToggle) // ПРИ СКРОЛЛЕ
+// });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ МАСКА ДЛЯ ИНПУТОВ (https://github.com/RobinHerbots/Inputmask)
+const inputMask = () => {
+    $(".js-maskPhone").inputmask({
+        mask: "+7 999 999 99 99",
+        clearIncomplete: true
+    });
+    $('.email').inputmask({
+        mask: "*{1,20}[.*{1,20}]@*{1,20}.*{2,4}",
+        clearIncomplete: true
+    //     greedy: false,
+    //     onBeforePaste: function (pastedValue, opts) {
+    //         pastedValue = pastedValue.toLowerCase();
+    //         return pastedValue.replace("mailto:", "");
+    //     },
+    //     definitions: {
+    //         '*': {
+    //             validator: "[0-9A-Za-z-а-я-]",
+    //             casing: "lower"
+    //         }
+    //     }
+    });
+    $(".js-maskDate").inputmask({
+        mask: "99/99/9999",
+        clearIncomplete: true,
+        'placeholder': 'dd/mm/yyyy'
+    });
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
+const sliders = () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+    
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
+const map = () => {
+
+    setTimeout(function() {
+        var headID = document.getElementsByTagName("body")[0];         
+        var newScript = document.createElement('script');
+        newScript.type = 'text/javascript';
+        newScript.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
+        headID.appendChild(newScript);
+    }, 3000);
+    setTimeout(function() {
+            var myMap = new ymaps.Map("map", {
+            center: [55.917879, 37.806326],
+            zoom: 13,
+            controls: ['smallMapDefaultSet']
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+        myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point"
+            },
+        });
+        myMap.geoObjects
+            .add(myGeoObject)
+            .add(new ymaps.Placemark([55.917879, 37.806326], {
+                balloonContent: '<strong></strong>',
+                iconCaption: 'М.О., г. Королев, ул. Ленина 12'
+            }, {
+                preset: 'islands#blueCircleDotIconWithCaption',
+                iconCaptionMaxWidth: '200'
+            }));
+
+        myMap.setType('yandex#publicMap');
+
+        myMap.behaviors.disable('scrollZoom');
+        //на мобильных устройствах... (проверяем по userAgent браузера)
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            //... отключаем перетаскивание карты
+            myMap.behaviors.disable('drag');
+        }
+    }, 4000);
+
+}
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INIT
+menu()
